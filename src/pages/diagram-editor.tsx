79947cdbe33222110d7cda3d9f6ad8f1dba22ef7
Terry Bai <z5430739@ad.unsw.edu.au>
2024-03-21 12:52:51 +1100

    add toolbar

--- src/pages/diagram-editor.tsx
+++ src/pages/diagram-editor.tsx
@@ -5,6 +5,21 @@ import { Stencil } from '@antv/x6-plugin-stencil'
 import { stencil_group, custom_nodes, custom_group } from '../components/nodes'
 import { ContextMenuTool } from '../components/context-menu'
 import { Group } from '../components/group'
+import { Menu, Toolbar } from '@antv/x6-react-components'
+import '@antv/x6-react-components/es/menu/style/index.css'
+import '@antv/x6-react-components/es/toolbar/style/index.css'
+import '../App.css';
+import {
+  ZoomInOutlined,
+  ZoomOutOutlined,
+  RedoOutlined,
+  UndoOutlined,
+  DeleteOutlined,
+  BoldOutlined,
+  ItalicOutlined,
+  StrikethroughOutlined,
+  UnderlineOutlined,
+} from '@ant-design/icons'
 
 /*
 TODO:
@@ -14,11 +29,14 @@ TODO:
 [x] Collapse/Expand the Parent Node: https://x6.antv.antgroup.com/en/examples/node/group/#collapsable
 [x] Detaching node: https://x6.antv.antgroup.com/en/examples/edge/tool/#context-menu
 [x] Ajusting arrowheads: https://x6.antv.antgroup.com/en/examples/edge/tool#arrowheads
+[ ] Toobar
 [ ] Export configurations
 [ ] zIndex issue
-[ ] Toobar: 
 [ ] Updating edges: https://x6.antv.antgroup.com/tutorial/basic/events
 */
+
+const Item = Toolbar.Item // eslint-disable-line
+const ToolbarGroup = Toolbar.Group // eslint-disable-line
 
 export default class DiagramEditor extends React.Component<{openDrawer : Function}> {
   private container: HTMLDivElement
@@ -265,6 +283,56 @@ export default class DiagramEditor extends React.Component<{openDrawer : Functio
       if (isNew) {
         const sourceNode = edge.getSourceNode()
         edge.setSource(sourceNode)
+        edge.setAttrs({
+          absoluteLabel: {
+            text: '150',
+            atConnectionLength: 20,
+          },
+        })
+        console.log(edge)
+        // edge.setLabels([
+        //   {
+        //     markup: [
+        //       {
+        //         tagName: 'rect',
+        //         selector: 'labelBody',
+        //       },
+        //       {
+        //         tagName: 'text',
+        //         selector: 'labelText',
+        //       },
+        //     ],
+        //     attrs: {
+        //       labelText: {
+        //         text: 'Label 1',
+        //         fill: '#ffa940',
+        //         textAnchor: 'middle',
+        //         textVerticalAnchor: 'middle',
+        //       },
+        //       labelBody: {
+        //         ref: 'labelText',
+        //         refX: -8,
+        //         refY: -5,
+        //         refWidth: '100%',
+        //         refHeight: '100%',
+        //         refWidth2: 16,
+        //         refHeight2: 10,
+        //         stroke: '#ffa940',
+        //         fill: '#fff',
+        //         strokeWidth: 2,
+        //         rx: 5,
+        //         ry: 5,
+        //       },
+        //     },
+        //     position: {
+        //       distance: 0.3,
+        //       args: {
+        //         keepGradient: true,
+        //         ensureLegibility: true,
+        //       },
+        //     },
+        //   },
+        // ])
       }
     })
 
@@ -285,11 +353,37 @@ export default class DiagramEditor extends React.Component<{openDrawer : Functio
     this.stencilContainer = container
   }
 
+  onClick = (name: string) => {
+    console.log(`${name} clicked`, 10)
+  }
+
   render() {
     return (
-      <div className="stencil-app">
-        <div className="app-stencil" ref={this.refStencil} />
-        <div className="app-content" ref={this.refContainer} />
+      <div>
+        <Toolbar className="toolbar" onClick={this.onClick} >
+          <ToolbarGroup>
+            <Item name="zoomIn" tooltip="Zoom In (Cmd +)" icon={<ZoomInOutlined />} />
+            <Item name="zoomOut" tooltip="Zoom Out (Cmd -)" icon={<ZoomOutOutlined />} />
+          </ToolbarGroup>
+          <ToolbarGroup>
+            <Item name="undo" tooltip="Undo (Cmd + Z)" icon={<UndoOutlined />} />
+            <Item name="redo" tooltip="Redo (Cmd + Shift + Z)" icon={<RedoOutlined />} />
+          </ToolbarGroup>
+          <ToolbarGroup>
+            <Item name="delete" icon={<DeleteOutlined />} disabled={true} tooltip="Delete (Delete)" />
+          </ToolbarGroup>
+          {/* <ToolbarGroup>
+            <Item name="bold" icon={<BoldOutlined />} active={true} tooltip="Bold (Cmd + B)" />
+            <Item name="italic" icon={<ItalicOutlined />} tooltip="Italic (Cmd + I)" />
+            <Item name="strikethrough" icon={<StrikethroughOutlined />} tooltip="Strikethrough (Cmd + Shift + x)" />
+            <Item name="underline" icon={<UnderlineOutlined />} tooltip="Underline (Cmd + U)" />
+          </ToolbarGroup> */}
+        </Toolbar>
+        <div className="stencil-app">
+          <div className="app-stencil" ref={this.refStencil} />
+          <div className="app-content" ref={this.refContainer}>
+          </div>
+        </div>
       </div>
     )
   }
---
+++ src/pages/memory-editor.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+
+export default class MemoryEditor extends React.Component {
+
+    render() {
+        return <div id="mem-bar">Memory Bar</div>
+    }
+}
