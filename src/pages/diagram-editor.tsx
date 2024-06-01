afbc634dc6872f7ac8136126744c9bb906c09621
Terry Bai <terry.z.bai@gmail.com>
2024-06-01 10:27:17 +1000

    remove interrupts from the main diagram

--- src/pages/diagram-editor.tsx
+++ src/pages/diagram-editor.tsx
@@ -131,6 +131,8 @@ export const DiagramEditor = () => {
 
   const editSDF = () => {
     setSDFEditorOpen(true)
+
+    console.log(globalGraph.toJSON())
   }
 
   const updateMappings = () => {
@@ -668,7 +670,7 @@ export const DiagramEditor = () => {
         onCancel={() => setSDFEditorOpen(false)}
         width={1000}
       >
-        <textarea value={SDFContent(globalGraph?.toJSON().cells, MRs)} readOnly style={ {width: '100%', height: '500px'} }></textarea>
+        <textarea value={SDFContent(globalGraph?.toJSON().cells, MRs)} style={ {width: '100%', height: '500px'} }></textarea>
       </Modal>
     </div>
   )
